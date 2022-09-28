04536512cb20ede4b35a85063d545b100e38337c
BaurNN <studentsoftpu@gmail.com>
2022-09-28 11:40:05 +0300

    add test model User

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+
+    fixtures = [
+        'test_user.json'
+    ]
+
+    def setUp(self):
+        # simulate user logging for auth.system
+        user_logged = get_user_model().objects.first()
+        self.client.force_login(user_logged)
+
+    def test_log_in_out(self):
+        # check response status, template
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='news/login.html')
+        # check login with true login&pass
+        register_user = {'username': 'test',
+                    'email: testmail@gmail.com'
+                    'password1': '123456test1',
+                    'password2': '123456test1'}
+        self.client.post(reverse('register'), register_user)
+        response = self.client.post(reverse('login'), {'username': 'test', 'password': '123456test1'})
+        self.assertEqual(response.status_code, 200)
+        # check login with false login&pass
+        response = self.client.post(reverse('login'), {'username': 'test2', 'password': '123458test'})
+        self.assertEqual(response.status_code, 200)
+        # check logout
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+
+    def test_register_user(self):
+        # check response status, template
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='news/register.html')
+        # check add user
+        register_user = {'username': 'test',
+                    'email': 'testmail@gmail.com',
+                    'password1': '123456test1',
+                    'password2': '123456test1'}
+
+        response = self.client.post(reverse('register'), register_user)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+        # check user added in database
+        user = get_user_model().objects.get(username=register_user['username'])
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(get_user_model().objects.count(), 2)
+        # check add user with same username
+        response = self.client.post(reverse('register'), register_user)
+        self.assertFormError(response, 'form', 'username', 'Пользователь с таким именем уже существует.')
