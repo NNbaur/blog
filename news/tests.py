6c22b02af08b3c5fea55ff45391f4d6a90c0ae41
BaurNN <studentsoftpu@gmail.com>
2022-09-28 12:55:04 +0300

    added test contact func

--- news/tests.py
+++ news/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from captcha.models import CaptchaStore
 
 
 class UserTestCase(TestCase):
-
     fixtures = [
         'test_user.json'
     ]
@@ -56,3 +56,58 @@ class UserTestCase(TestCase):
         # check add user with same username
         response = self.client.post(reverse('register'), register_user)
         self.assertFormError(response, 'form', 'username', 'Пользователь с таким именем уже существует.')
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Ошибка регистрации')
+
+
+class ContactTestCase(TestCase):
+    fixtures = [
+        'test_user.json'
+    ]
+
+    def setUp(self):
+        # simulate user logging for auth.system
+        user_logged = get_user_model().objects.first()
+        self.client.force_login(user_logged)
+
+    def test_contact(self):
+        # check response status, template
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='news/contact.html')
+
+        # check valid contact form is correct
+        # check captcha is loaded
+        captcha_count = CaptchaStore.objects.count()
+        self.failUnlessEqual(captcha_count, 1)
+        # get captcha data from CaptchaStore
+        captcha = CaptchaStore.objects.all()[0]
+        # fill contact form, captcha_0 - captcha task, captcha_1 - captcha solution
+        contact = {
+            'subject': 'test_subject',
+            'content': 'test_content',
+            'mail': 'testmail@gmail.com',
+            'captcha_0': captcha.hashkey,
+            'captcha_1': captcha.response,
+        }
+        response = self.client.post(reverse('contact'), contact)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+
+        # check invalid contact form
+        contact_invalid = {
+            'subject': '',
+            'content': 'test_content',
+            'mail': 'testmail@gmail.com',
+            'captcha_0': captcha.hashkey,
+            'captcha_1': captcha.response,
+        }
+        response = self.client.post(reverse('contact'), contact_invalid)
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Ошибка проверки')
+
+
+
+
+
+
